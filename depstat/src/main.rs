c9139089859485c9f67e038ed2d64780a9d04421
Hongxu Xu <h445xu@uwaterloo.ca>
2025-07-09 17:25:08 +0000

    feat(depstat): add cli

--- depstat/src/main.rs
+++ depstat/src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+use depreduce::{
+    graph::bazel_xml_parser::parse_bazel_xml, stats::rebuild_cost::RebuildCostCalculator,
+};
+use utils::get_bazel_query;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about=None)]
+struct Args {
+    #[arg(short, long)]
+    workspace: String,
+
+    #[arg(short, long)]
+    deps_only: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let xml_str = get_bazel_query(&args.workspace);
+    let query = parse_bazel_xml(&xml_str).unwrap();
+    let graph = query.to_dep_graph(args.deps_only).unwrap();
+    let original_cost = RebuildCostCalculator::new(&graph).calculate_rebuild_cost_sum();
+    println!("Rebuild cost: {}", original_cost);
 }
