409628219a6244363535a817ac32b8c076b8a6d7
Hongxu Xu <h445xu@uwaterloo.ca>
2025-08-11 19:08:22 +0000

    fix(depreduce): remove disable dep lifting in rocketmq exp

--- scripts/experiments/rocketmq.sh
+++ scripts/experiments/rocketmq.sh
@@ -3,6 +3,5 @@ export RUST_BACKTRACE=1
     -w /data/h445xu/repo/bazel-repos/cloned_repos/rocketmq \
     -c $PWD/scripts/experiments/rocketmq-build.sh \
     --deps-only \
-    --disable-dependency-lifting \
     --config $PWD/scripts/experiments/rocketmq.toml \
     --output rocketmq-output > rocketmq.stdout 2>rocketmq.stderr
