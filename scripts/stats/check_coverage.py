5b744b2d759bdda606474331e60bccdafa1e37e1
Hongxu Xu <h445xu@uwaterloo.ca>
2025-12-17 02:11:14 +0000

    feat(stats): collect optimized and revertible commits

--- scripts/stats/check_coverage.py
+++ scripts/stats/check_coverage.py
@@ -3,12 +3,14 @@ import os
 import glob
 import argparse
 
-from pydantic import BaseModel
-from scripts.stats.collect_commits import Result
+from git import Repo
 
-
-class RunArgs(BaseModel):
-    result_dir: str
+from scripts.stats.collect_commits import (
+    RunArgs,
+    Result,
+    get_next_n_commits_from_base,
+    switch_to_commit,
+)
 
 
 def parse_args():
@@ -45,6 +47,12 @@ def extract_targets(target_str: str) -> list[str]:
 def main():
     args = parse_args()
 
+    repo = Repo(args.repo_path)
+    switch_to_commit(repo, args.base_commit)
+    commits = get_next_n_commits_from_base(
+        repo, args.default_branch, args.base_commit, args.n_commits
+    )
+
     CHANGED_REBUILD_SETS_JSON_FILE_NAME = "changed_rebuild_sets.json"
 
     with open(
@@ -57,7 +65,8 @@ def main():
 
     n = 0
     optimized_commits = []
-    for path in glob.glob(os.path.join(args.result_dir, "*.json")):
+    for commit in commits:
+        path = os.path.join(args.result_dir, f"{commit.hexsha}.json")
         if path.endswith(CHANGED_REBUILD_SETS_JSON_FILE_NAME):
             continue
 
@@ -80,6 +89,30 @@ def main():
                 break
 
     print(f"Optimized Commits/Total: {len(optimized_commits)}/{n}")
+
+    with open(os.path.join(args.result_dir, "optimized_commits.txt"), "w") as f:
+        for commit in optimized_commits:
+            f.write(f"{commit}\n")
+
+    revertible_commits = []
+    for commit in optimized_commits:
+        switch_to_commit(repo, commit)
+        try:
+            repo.git.execute(
+                f"git revert --no-commit {args.base_commit}".split(),
+                stdout_as_string=True,
+            )
+            repo.git.execute("git revert --abort".split(), stdout_as_string=True)
+            revertible_commits.append(commit)
+            if len(revertible_commits) == 10:
+                break
+        except:
+            repo.git.execute("git revert --abort".split(), stdout_as_string=True)
+            break
+
+    with open(os.path.join(args.result_dir, "revertible_commits.txt"), "w") as f:
+        for commit in revertible_commits:
+            f.write(f"{commit}\n")
 
 
 if __name__ == "__main__":
